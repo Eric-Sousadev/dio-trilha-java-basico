c28bd78e0993fec7f071dc487c875e12a5f5a4cc
Eric Sousa <eric.sousard021@gmail.com>
2025-03-21 18:21:04 -0300

    Tipos Variaveis e outros

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,40 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        
+        
+         //tipos primitivos
+
+        //byte: Um tipo numérico inteiro com 8 bits de tamanho 
+          // Tentando ultrapassar o limite do byte (vai dar erro!)
+            // byte numeroGrande = 130; // ERRO: valor fora do intervalo permitido
+            byte idade = 25;
+
+        //short: Um tipo numérico inteiro com 16 bits de tamanho 
+            short numeroCurto = 1;
+
+        //int: Um tipo numérico inteiro com 32 bits de tamanho 
+            int numero = 12; 
+
+        //long: Um tipo numérico inteiro com 64 bits de tamanho e tem que ter o "L"
+            long cpf = 12345678900L;
+
+        //boolean: Um tipo que aceita apenas os valores true ou false 
+            boolean doadorOrgao = true;
+
+        //char: Um tipo que pode receber caracteres, valores literais do tipo int e unicode 
+            char sexo = 'M';
+
+        //float: Um tipo numérico de ponto flutuante com 4 bytes de tamanho e tem que colocar o "F"
+            float pi = 3.14f;
+
+        //double: Um tipo numérico de ponto flutuante com 8 bytes de tamanho 
+            double peso = 68.5;
+
+        //constante
+            final double VALOR_DE_PI = 3.14;
+
+        //representar texto
+            String meuNome = "Eric";
     }
 }
